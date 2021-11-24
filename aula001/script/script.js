2ff78a8eb4641b36fcff7ddf8a3177ba15356c65
Ernold <devernold2@gmail.com>
2021-11-23 21:41:13 -0300

    atualizado

--- aula001/script/script.js
+++ aula001/script/script.js
@@ -28,3 +28,84 @@ function sair(){
     caixa.style.color = 'black';
     caixa.style.textTransform = 'uppercase'
 }
+
+// Pegar a data atual do computador
+let agora = new Date();
+let diaSemana = agora.getDay();
+let data = document.getElementById('data');
+
+switch(diaSemana){
+    //case 0:
+        //data.innerHTML = 'Domingo';
+        //data.style.background = 'black';
+    //break;
+
+    //case 1:
+        //data.innerHTML = 'Segunda-feira';
+        //data.style.background = 'red';
+    //break;
+
+    //case 2:
+        //data.innerHTML = 'Terça-feira';
+        //data.style.background = 'green';
+    //break;
+
+    //case 3:
+        //data.innerHTML = 'Quarta-feira';
+        //data.style.background = 'orange';
+    //break
+
+    //case 4:
+        //data.innerHTML = 'Quinta-feira';
+        //data.style.background = 'purple';
+    //break;
+
+    //case 5:
+        //data.innerHTML = 'Sexta-feira';
+        //data.style.background = 'grey';
+    //break;
+
+    //case 6:
+        //data.innerHTML = 'Sabado';
+        //data.style.background = 'blue';
+    //break;
+}
+
+if(diaSemana == 0){
+    data.innerHTML = ('Domingo');
+    data.style.background = 'black';
+} else if(diaSemana == 1){
+    data.innerHTML = ('Segunda-feira');
+    data.style.background = ('blue')
+} else if(diaSemana == 2){
+    data.innerHTML = ('Terça-feira');
+    data.style.background = ('purple');
+} else if(diaSemana == 3){
+    data.innerHTML = ('Quarta-feira');
+    data.style.background = ('red')
+} else if(diaSemana == 4){
+    data.innerHTML = ('Quinta-feira');
+    data.style.background = ('black');
+    data.style.color = ('red');
+} else if(diaSemana == 5){//
+    data.innerHTML = ('Sexta-feira');
+    data.style.background = ('green');
+} else if(diaSemana == 6){
+    data.innerHTML = ('Sabado');
+    data.style.background = ('grey');
+} else{
+    data.innerHTML = ('Data inválida');
+    data.style.background = ('white');
+    data.style.color = ('black');
+}
+
+let contador = 0;
+let dominio = document.getElementById('dominio');
+
+while(contador < 300){
+    contador++;
+    dominio.innerHTML = (dominio);
+    console.log(dominio);
+}
+
+
